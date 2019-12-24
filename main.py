5af4ad8ea76434b650d5a8bba5d8becd6b6b8ec7
Ran Galun <galun.ran@gmail.com>
2019-12-24 18:50:30 +0200

    improve graph plotting

--- main.py
+++ main.py
@@ -26,19 +26,22 @@ def calc_probs_all_paths(all_paths, graph, edges_in_path, MSP=0.5):
     return number_of_found_paths
 
 
-def draw_final_graph(graph, edges_in_paths):
+def draw_final_graph(graph, edges_in_paths, ego_node, target_node):
     g = nx.DiGraph(graph.get_dict_graph())
     pos = nx.spring_layout(g)
+    colors_nodes_dict = {ego_node: "r", target_node: "g"}
     colors_edges = ["r" if edge in edges_in_paths else "b" for edge in g.edges()]
+    colors_nodes = [colors_nodes_dict.get(node, "b") for node in g.nodes]
     plt.figure()
-    nx.draw_networkx(g, pos=pos, node_size=[5000] * len(g.nodes()), with_labels=False)
     shifted_pos = {k: [v[0], v[1] + .04] for k, v in pos.items()}
     node_label_handles = nx.draw_networkx_labels(g, pos=shifted_pos,
-                                                 labels=graph.get_node_labels())
+                                                 labels=graph.get_node_labels(), font_size=8)
     [label.set_bbox(dict(facecolor='white', edgecolor='none')) for label in
      node_label_handles.values()]
     nx.draw_networkx_edge_labels(g, pos=pos, edge_labels=graph.get_edge_labels())
     nx.draw_networkx_edges(g, pos, edge_color=colors_edges)
+    nx.draw_networkx_nodes(g, pos, node_color=colors_nodes)
+    nx.draw_networkx(g, pos=pos, node_size=[3000] * len(g.nodes()), with_labels=False)
     plt.show()
 
 
@@ -55,12 +58,12 @@ def main():
             random_connection = choice(list(graph.get_connections().keys()))
             all_paths = graph.find_all_paths(random_connection[0], random_connection[1])
             MSPs_dict[MSP] += calc_probs_all_paths(all_paths, graph, edges_in_paths, MSP=MSP)
-            draw_final_graph(graph, edges_in_paths)
+            draw_final_graph(graph, edges_in_paths, random_connection[0], random_connection[1])
     sorted_MSPs_keys = [key for (key, value) in sorted(MSPs_dict.items())]
     sorted_MSPs_values = [value for (key, value) in sorted(MSPs_dict.items())]
     plt.plot(sorted_MSPs_keys, sorted_MSPs_values, 'bo')
     plt.xlabel('MSP values')
-    plt.ylabel('number of paths found in 5 runs')
+    plt.ylabel('number of paths found in 100 runs')
     plt.show()
 
 
